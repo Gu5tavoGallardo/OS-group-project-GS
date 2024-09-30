353833511e9e522248fd0d8e878113c2ef69514d
Gu5tavoGallardo <g_gallargo01@icloud.com>
2024-09-30 12:36:02 -0500

    i finish the end part

--- Main_OS.cpp
+++ Main_OS.cpp
@@ -6,20 +6,27 @@ int main(){
 int userInput;
 
 cout << "welcome to the operating system" << endl;
-cout << "Plaese input either a 1 to continue or 0 to end the program" << endl;
-cin >> userInput; 
+while(true){
+  cout << "Plaese input either a 1 to continue or 0 to end the program" << endl;
+  cin >> userInput;
 
+    // This function is so the users dont put in letters or words 
     if(cin.fail()){
-      cout << "The input does not work" << endl;
-    exit(1);  
+      cout << " you can only put a 0 or 1" << endl;
+      
     }
-
-    if(userInput == 0)
-    cout << "You have inputed 0 the program will end now" << endl;
-    exit(0);
-
-  if else(userInput)
-
+    // if the user puts 0 they will end the process 
+    if(userInput == 0){
+      cout << "You have inputed 0 the program will end now" << endl;
+    exit(0);   
+    }
+    // if they put a 1 they can continue the process 
+    else if(userInput == 1){
+      cout << "you can continue with the program" << endl;
+    }
+          }
+    return 0;
+    
 
 
 
